7fe2d8ff067e6bebe5370f8c5dd123093f2447e4
BuildTools <clayloneman@gmail.com>
2020-09-29 16:24:14 -0500

    finished project

---
+++ usercard/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./App.scss";
+import Usercard from './components/usercard'
+class App extends React.Component {
+  state = {
+    data: [],
+    followerdata: [],
+    user: "rabithole"
+  };
+
+  componentDidMount() {
+    this.fetchUser(this.state.user);
+  }
+
+
+
+  fetchUser = (user) => {
+    fetch(`https://api.github.com/users/${user}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.fetchFollowers(data.followers_url)
+        this.setState({data})
+      })
+      .catch((err) => console.log("error: ", err));
+  };
+
+  fetchFollowers = (url) => {
+    fetch(`${url}`)
+      .then((res) => res.json())
+      .then((followerdata) => {
+        this.fetchFollowersDetail(followerdata)
+      })
+      .catch((err) => console.log("error: ", err));
+  };
+
+  fetchFollowersDetail = (data) => {
+    data.map((user) => (
+      fetch(`${user.url}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({followerdata:this.state.followerdata.concat(data)});
+      })
+      .catch((err) => console.log("error: ", err))
+    ))
+    
+  };
+  render() {
+    return (
+      <div>
+        <h1 className='center'>GitHub Profile</h1>
+        <div className='cardContainer'>
+        <Usercard data={this.state.data}/>
+        </div>
+        <h2 className='center'>Followers:</h2>
+        <div className="cardContainer">
+        {this.state.followerdata.map((user, x) => (
+            <Usercard key={x} data={user} />
+          ))}
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ usercard/src/components/usercard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../App.scss";
+
+class Usercard extends React.Component {
+    render() {
+        const user = this.props.data;
+        return (
+            <div className='card'>
+                <div className='titleBar'>
+                    <div className='name'>
+                        <h1>{user.name == undefined ? user.login : ''}{user.name}</h1>
+                    </div>
+                    <div className='profile'>
+                        <img className='profileImg' src={user.avatar_url} alt={user.name} />
+                    </div>
+
+                </div>
+                <div className='details'>
+                    <p><i className="fas fa-id-badge" /> {user.login}</p>
+                    <p><i className="fas fa-location-arrow" /> {user.location}</p>
+                    <p><i className="fas fa-user-friends"/> {user.followers} | {user.following}</p>
+                </div>
+                <div className = 'center'>
+                <a href={user.html_url} rel="noopener noreferrer" target = '_blank'>Open Profile</a>
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default Usercard;
